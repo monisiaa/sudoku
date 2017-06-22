ba4061453ed0751e8bdd372aba99c4852f58fe51
Monika Potrykus <monikapotrykus94@gmail.com>
2017-06-22 14:36:19 +0200

    Add 2 button (Hwo to Play & Start Game) to Sudoku and functionality the first button.

---
+++ Sudoku/Form1.Designer.cs
@@ -0,0 +1,74 @@
+﻿namespace Sudoku
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Wymagana zmienna projektanta.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Wyczyść wszystkie używane zasoby.
+        /// </summary>
+        /// <param name="disposing">prawda, jeżeli zarządzane zasoby powinny zostać zlikwidowane; Fałsz w przeciwnym wypadku.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kod generowany przez Projektanta formularzy systemu Windows
+
+        /// <summary>
+        /// Wymagana metoda obsługi projektanta — nie należy modyfikować 
+        /// zawartość tej metody z edytorem kodu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.bHow_Play = new System.Windows.Forms.Button();
+            this.bStart = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // bHow_Play
+            // 
+            this.bHow_Play.Location = new System.Drawing.Point(65, 36);
+            this.bHow_Play.Name = "bHow_Play";
+            this.bHow_Play.Size = new System.Drawing.Size(132, 62);
+            this.bHow_Play.TabIndex = 0;
+            this.bHow_Play.Text = "How to play";
+            this.bHow_Play.UseVisualStyleBackColor = true;
+            this.bHow_Play.Click += new System.EventHandler(this.bHow_Play_Click);
+            // 
+            // bStart
+            // 
+            this.bStart.Location = new System.Drawing.Point(65, 135);
+            this.bStart.Name = "bStart";
+            this.bStart.Size = new System.Drawing.Size(132, 62);
+            this.bStart.TabIndex = 1;
+            this.bStart.Text = "Start game";
+            this.bStart.UseVisualStyleBackColor = true;
+            this.bStart.Click += new System.EventHandler(this.bStart_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.bStart);
+            this.Controls.Add(this.bHow_Play);
+            this.Name = "Form1";
+            this.Text = "Sudoku";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button bHow_Play;
+        private System.Windows.Forms.Button bStart;
+    }
+}
+
---
+++ Sudoku/Form1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Sudoku
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void bHow_Play_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Sudoku is played on 9x9 board divided into 3x3 smaller areas. " +
+                "At the beginning of the game some of the Sudoku board boxes are already filled with numbers. " +
+                "The object of the game is to complete the remaining board areas with numbers from 1 to 9 (one digit in each field)," +
+                " with the following rules: \n" +
+                "1. Each digit can only appear once per line. \n" +
+                "2. Each digit can appear only once in each column. \n" +
+                "3. Each digit can appear only once in each area 3x3. \n");
+        }
+
+        private void bStart_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
